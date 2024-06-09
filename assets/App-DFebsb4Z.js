cd157fb82d64b542be5795f5f65ec994a625fe6e
WakilMahmud <wakilmahmud30@gmail.com>
2024-06-09 12:15:37 +0600

    Updates

---
+++ assets/App-DFebsb4Z.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-Cy5TcOAp.js";const e=()=>t.jsx(s,{});export{e as default};
---
+++ assets/index-CdDZkYJ3.js
@@ -0,0 +1,1 @@
+import{j as t,S as e,O as s}from"./index-Cy5TcOAp.js";const r=()=>t.jsx(e,{justifyContent:"center",alignItems:"center",minHeight:"100vh",children:t.jsx(s,{})});export{r as default};
